b112fc8a99d6100177aefa12caed143c2187fb4b
Rakshana <142417874+Rakshana77@users.noreply.github.com>
2025-04-16 14:28:28 +0530

    Update Hero.js

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,6 +1,6 @@
 import HeroImg from '../assets/hero.png';
 import { AiOutlineGithub, AiOutlineLinkedin } from "react-icons/ai";
-
+const resumeLink = "https://drive.google.com/file/d/195FQzVafGVLvzYj7VuTGMge4MLxgNuak/view?usp=sharing";
 export default function Hero() {
     const config = {
         subtitle: 'Full-stack developer',
@@ -17,6 +17,20 @@ export default function Hero() {
                     Hi, <br/> I'm <span className='text-white'>S.</span>RAKSHANA 
                     <p className='text-2xl'>{config.subtitle}</p>
                 </h1>
+                
+                {/* Resume Button */}
+                <div className="mt-5">
+                    <a 
+                        href={resumeLink} 
+                        target="_blank" 
+                        rel="noopener noreferrer" 
+                        className="bg-secondary text-white px-6 py-3 rounded-lg font-bold hover:bg-white hover:text-primary transition duration-300"
+                    >
+                        View Resume
+                    </a>
+                </div>
+
+                {/* Social Links */}
                 <div className='flex py-10'>
                     <a
                         href={config.social.github}
