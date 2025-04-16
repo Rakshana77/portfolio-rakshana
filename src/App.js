f62985ef7f69791acb0a579e3b06f1b227f6af14
Rakshana <142417874+Rakshana77@users.noreply.github.com>
2025-04-16 14:27:52 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Footer from './components/Footer';
 import Header from './components/Header';
 import Hero from './components/Hero';
 import Projects from './components/Projects';
-import Resume from './components/Resume';
+
 import Skills from './components/Skills';
 
 function App() {
@@ -18,7 +18,7 @@ function App() {
       <Skills />
       <EducationAndCertificates />
         <Projects/>
-        <Resume/>
+     
         <Contact/>
         <Footer/>
     </div>
